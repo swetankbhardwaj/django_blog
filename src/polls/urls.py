aa043e800f3323d1ab29286dde4285052dbb69fd
Swetank <swetank311@gmail.com>
2017-01-25 14:02:56 +0530

    After inherited templates

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', "polls.views.post_list"),
+	url(r'^$', "polls.views.post_list", name="list"),
     url(r'^create/$', "polls.views.post_create"),
     url(r'^(?P<id>\d+)/$', "polls.views.post_detail" ,name="detail"),
-    url(r'^update/$', "polls.views.post_update"),
-    url(r'^delete/$', "polls.views.post_delete"),
+    url(r'^(?P<id>\d+)/edit/$', "polls.views.post_update", name='update'),
+    url(r'^(?P<id>\d+)/delete/$', "polls.views.post_delete"),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,31 +1,61 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Post
- 
-# Create your views here.
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import PostForm
+from .models import Post
 
 def post_create(request):
-	return HttpResponse("<h1>Create</h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		# message success
+		messages.success(request, "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+		
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
-def post_detail(request,id=None): #retrieve
-	instance = get_object_or_404(Post, id=id) 
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
 	context = {
 		"title": instance.title,
-		"instance": instance
+		"instance": instance,
 	}
 	return render(request, "post_detail.html", context)
 
-def post_list(request): #list items
-	queryset=Post.objects.all()
+def post_list(request):
+	queryset = Post.objects.all()
 	context = {
-		"object_list":queryset,
+		"object_list": queryset, 
 		"title": "List"
 	}
-	return render(request, "index.html", context)
+	return render(request, "post_list.html", context)
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
 
-def post_delete(request):
-	return HttpResponse("<h1>Delete</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form":form,
+	}
+	return render(request, "post_form.html", context)
+
+
+
+def post_delete(request,id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request, "Successfully deleted")
+	return redirect("posts:list")
