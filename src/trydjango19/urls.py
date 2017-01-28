9accaa9ec3864f0f435fc1d485b3772ed99a552f
Swetank <swetank311@gmail.com>
2017-01-28 16:32:46 +0530

    Bootstrap added

--- src/trydjango19/urls.py
+++ src/trydjango19/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include("polls.urls",namespace="posts")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
