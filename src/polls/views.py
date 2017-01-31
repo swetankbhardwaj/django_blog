e2b036e7ed04c58d5617624af0d4137c4ec53bd3
Swetank <swetank311@gmail.com>
2017-01-31 18:02:19 +0530

    After Image field

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -28,17 +28,32 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
+
 	context = {
 		"object_list": queryset, 
-		"title": "List"
+		"title": "List",
+		"page_request_var": page_request_var
 	}
 	return render(request, "post_list.html", context)
 
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
